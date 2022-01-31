6923a4a665918061e68c19a2f900be4622ea667a
Abylay Bazarbayev <abazarbayev@1cb.kz>
2022-01-31 18:18:14 +0600

    change user

--- config/adminDB.config.js
+++ config/adminDB.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     HOST: "localhost",
     PORT: 3306,
-    DB_NAME: "p-326169_first",
-    USER: "p-326169_first",
+    DB_NAME: "p-326169_admin",
+    USER: "p-326169_admin",
     PASSWORD: "xojb_GBiOCb2",
-    DIALECT: "mysql",
+    DIALECT: "mariadb",
     pool: {
       max: 5,
       min: 0,
