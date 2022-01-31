7d408727ac831f418c7a3440f6dad7eed2e9b935
Abylay Bazarbayev <abazarbayev@1cb.kz>
2022-01-31 16:44:36 +0600

    check

--- apps/services/controller.js
+++ apps/services/controller.js
@@ -2,11 +2,7 @@ const db = require('../../models');
 const Service = db.service;
 
 exports.findAll = (req, res) => {
-    // Service.findAll()
-    //     .then(res.send)
-    //     .catch(err => res.status(500).json({error: err, status: "ehh"}))
-    db.sequelize
-        .authenticate()
+    Service.findAll()
         .then(res.send)
         .catch(err => res.status(500).json({error: err, status: "ehh"}))
 }
--- config/adminDB.config.js
+++ config/adminDB.config.js
@@ -3,6 +3,5 @@ module.exports = {
     DB_NAME: "p-326169_admin",
     USER: "p-326169_admin",
     PASSWORD: "xojb_GBiOCb2",
-    PORT: 3306,
     DIALECT: "mysql"
 };
