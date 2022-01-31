c9e581aa2b4bf686766121348818abcdd006a6c8
Abylay Bazarbayev <abazarbayev@1cb.kz>
2022-01-31 16:47:48 +0600

    port

--- config/adminDB.config.js
+++ config/adminDB.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     HOST: "localhost",
+    PORT: 3306,
     DB_NAME: "p-326169_admin",
     USER: "p-326169_admin",
     PASSWORD: "xojb_GBiOCb2",
-    DIALECT: "mysql"
+    DIALECT: "mysql",
 };
